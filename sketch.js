adf0ebbb1062b96555cc57882c2bfb328fdffeb4
maksonhan <max-ove@ya.ru>
2023-05-25 22:24:53 +0300

    metal flow without central part of detail

--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
 let tumblerDraw = false;
-let amountParticles = 700;
+let amountParticles = 600;
 let maxParticlesVertical = 25;
+let constSpeed = 1;
 
 //inizialization sample shape
-let SideA;
-let SideB;
+let SideX;
+let SideY;
 let b2;
 let a2;
 let b1;
@@ -13,15 +14,14 @@ let a1;
 class MetalParticle {
 
   constructor() {
-    // this.x = random(10, SideA-10);
-    // this.y = random(10, SideB-10);
     this.x;
     this.y;
     this.xFirst;
     this.yFirst;
     this.r = 3;
-    this.xSpeed = random(-0.5,0.5);
-    this.ySpeed = random(-0.5,0.5);
+    this.xSpeed;
+    this.ySpeed;
+    this.trace = [];
   }
 
   createParticle() {
@@ -29,29 +29,103 @@ class MetalParticle {
   }
 
   restartPos() {
-    // this.x = this.xFirst;
-    // this.y = this.yFirst;
-    this.xSpeed = 0;
-    this.ySpeed = 0;
+    this.x = this.xFirst;
+    this.y = this.yFirst;
+    // this.xSpeed = 0;
+    // this.ySpeed = 0;
   }
 
 
   moveParticle() {
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
-    if(this.x >= SideA || this.x <=  0) {     
+
+    //разбиеие движения по секторам
+    if(this.x < SideX - a1 & this.y < a2 ) {
+      if( this.x > a1 ) {
+        this.y += constSpeed;
+      }
+      else {
+        this.x += PullToVertex(this.x,this.y,a1,a2).dirX;
+        this.y += PullToVertex(this.x,this.y,a1,a2).dirY;
+      }
+    }
+
+    else if (this.x < SideX - a1 & this.y > SideY - a2) {
+      if( this.x > a1 ) {
+        this.y -= constSpeed;
+      }
+      else {
+        this.x += PullToVertex(this.x,this.y,a1,SideY - a2).dirX;
+        this.y += PullToVertex(this.x,this.y,a1,SideY - a2).dirY;
+      }
+    }
+
+    else if (this.x < SideX & this.y < SideY - a2) {
+      if( this.x > SideX - a1 & this.y > a2) {
+        this.x -= constSpeed;
+      }
+
+      else if(this.x <= a1) {
+        this.x += constSpeed;
+      }
+
+      else if(this.x > SideX-a1 & this.y < a2) {
+        this.x += PullToVertex(this.x,this.y,SideX-a1,a2).dirX;
+        this.y += PullToVertex(this.x,this.y,SideX-a1,a2).dirY;
+      }
+
+    }
+
+    else {
+      this.x += PullToVertex(this.x,this.y,SideX-a1,SideY - a2).dirX;
+      this.y += PullToVertex(this.x,this.y,SideX-a1,SideY - a2).dirY;
+    }
+
+
+
+    // this.x += this.xSpeed;
+    // this.y += this.ySpeed;
+    if(this.x >= SideX || this.x <=  0) {     
       this.restartPos();
     }
-    if(this.y >= SideA || this.y <=  0) {     
+    if(this.y >= SideY || this.y <=  0) {     
       this.restartPos();      
     }
 
+    // if( dist(this.xFirst, this.yFirst, this.x,this.y) >= constSpeed*100) {     
+    //   this.restartPos();      
+    // }
+
     //проверка на попадание частицы во внутренний контур. 
-    if(this.y <= SideB - a2 & this.x<= SideA - a1) {
+    if(this.y <= SideY - a2 & this.x <= SideX - a1) {
       if( this.x >= a1 & this.y >=  a2  ) this.restartPos();
-    }
+    }  
 
   }
+
+
+  makeTrace() {
+    push();
+    strokeWeight(0.3);
+    stroke('#EB5250');
+    line(this.x, this.y, this.xFirst, this.yFirst);
+    pop();
+  }
+
+  // testTrace() {
+
+  //   if(this.xSpeed != 0 || this.ySpeed != 0) {
+
+  //     for(let i=0; i< this.trace.length; i++) {
+  //       circle(this.trace[i].x, this.trace[i].y, 0.2);
+  //     }
+
+  //     let track = {x:this.x, y:this.y};
+  //     this.trace.push(track);
+  //   }
+  //   else return;
+    
+  // }
+
 
 }
 
@@ -69,7 +143,14 @@ function setup() {
     // particles[i].yFirst = particles[i].y;
   }
 
-  // setGrid(particles);
+  b2 =  document.getElementById('b2').value*10;
+  a2 =  b2-document.getElementById('a2').value*10;
+  b1 =  document.getElementById('b1').value*10;
+  a1 =  b1-document.getElementById('a1').value*10;
+  SideX =  b1*2;;
+  SideY =  b2*2;
+
+  setGrid(particles);
 
   noLoop();
 }
@@ -80,29 +161,30 @@ function draw() {
   fill('#BAC3D5');
   
   beginShape();
-  vertex(width/2 - SideA/2, height/2 - SideB/2);
-  vertex(width/2 + SideA/2, height/2 - SideB/2);
-  vertex(width/2 + SideA/2, height/2 + SideB/2);
-  vertex(width/2 - SideA/2, height/2 + SideB/2);
+  vertex(width/2 - SideX/2, height/2 - SideY/2);
+  vertex(width/2 + SideX/2, height/2 - SideY/2);
+  vertex(width/2 + SideX/2, height/2 + SideY/2);
+  vertex(width/2 - SideX/2, height/2 + SideY/2);
 
 
   beginContour();
-  vertex(width/2 - SideA/2 + a1, height/2 - SideB/2 + a2);
-  vertex(width/2 - SideA/2 + a1, height/2 + SideB/2 - a2);
-  vertex(width/2 + SideA/2 - a1, height/2 + SideB/2 - a2);
-  vertex(width/2 + SideA/2 - a1, height/2 - SideB/2 + a2);
+  vertex(width/2 - SideX/2 + a1, height/2 - SideY/2 + a2);
+  vertex(width/2 - SideX/2 + a1, height/2 + SideY/2 - a2);
+  vertex(width/2 + SideX/2 - a1, height/2 + SideY/2 - a2);
+  vertex(width/2 + SideX/2 - a1, height/2 - SideY/2 + a2);
   endContour();
 
   endShape(CLOSE);
 
-  translate(width/2 - SideA/2,height/2 - SideB/2);
+  translate(width/2 - SideX/2,height/2 - SideY/2);
   fill('red');
+
 
   for (let i = 0; i<particles.length; i++) {
     particles[i].createParticle();
     particles[i].moveParticle();
+    particles[i].makeTrace();
   }
-
   
 }
 
@@ -117,10 +199,10 @@ function setGrid(particles) {
 
   for (let i = 0; i<sliceParticles.length; i++) {
 
-    let posX = (a+1)*SideA/(devider+1);
-    let posY = (i+1)*SideB/(sliceParticles.length+1);
+    let posX = (a+1)*SideX/(devider+1);
+    let posY = (i+1)*SideY/(sliceParticles.length+1);
 
-    if(posY <= SideB - a2 & posX <= SideA - a1) {
+    if(posY <= SideY - a2 & posX <= SideX - a1) {
       //проверяем расположение частиц относительно внутренного контура, если попадают, то перепрыгиваем на следующую итерацию, не присваивая значения
       if( posX >= a1 & posY >=  a2  ) {
         sliceParticles[i].x = NaN; // Убираем частицы, которые попадают в новый внутренний контур при изменение параметров изделия
@@ -144,15 +226,17 @@ function setGrid(particles) {
 }
 
 function getShape(event) {
+
+    //проверяем на корректно введеные значения
+    if(!checkInp()) {event.preventDefault()};
+
     b2 =  document.getElementById('b2').value*10;
     a2 =  b2-document.getElementById('a2').value*10;
     b1 =  document.getElementById('b1').value*10;
     a1 =  b1-document.getElementById('a1').value*10;
-    SideA =  b1*2;;
-    SideB =  b2*2;
+    SideX =  b1*2;;
+    SideY =  b2*2;
 
-    //проверяем на корректно введеные значения
-    if(!checkInp(a1,b1,a2,b2)) {event.preventDefault()};
 
     setGrid(particles);
 
@@ -167,7 +251,7 @@ function getShape(event) {
 }
 
 
-function checkInp(a1,b1,a2,b2) {
+function checkInp() {
 
   //Проверка, на пустые инпуты
   if (Number(document.getElementById('a1').value) >= Number(document.getElementById('b1').value)) {
@@ -190,8 +274,6 @@ function resetSketch() {
   for(let i = 0; i<particles.length; i++) {
     particles[i].x = particles[i].xFirst;
     particles[i].y = particles[i].yFirst;
-    particles[i].xSpeed = random(-0.5,0.5);
-    particles[i].ySpeed = random(-0.5,0.5);
   }   
 }
 
@@ -200,8 +282,8 @@ function resetSketch() {
 //   stopDraw()
 
 //   for(let i = 0; i<particles.length; i++) {
-//     particles[i].x = random(10, SideA-10);
-//     particles[i].y = random(10, SideB-10);
+//     particles[i].x = random(10, SideX-10);
+//     particles[i].y = random(10, SideY-10);
 //   } 
 
 // }
@@ -220,225 +302,18 @@ function stopDraw() {
 }
 
 
-      
+function PullToVertex(x,y,hx,hy) {
+  let changespeed = {dirX: 0, dirY: 0};
+  let dis = dist(x,y,hx,hy);
+  let t = dis/constSpeed;
+  changespeed.dirX = abs(x-hx)/t;
+  changespeed.dirY = abs(y-hy)/t;
+
+  if(x-hx > 0) changespeed.dirX = -1*changespeed.dirX;
+  if(y-hy > 0) changespeed.dirY = -1*changespeed.dirY;
+
+  return changespeed;
+}
 
 
-// let flock;
-
-// function setup() {
-//   createCanvas(640, 360);
-//   createP("Drag the mouse to generate new boids.");
-
-//   flock = new Flock();
-//   // Add an initial set of boids into the system
-//   for (let i = 0; i < 2; i++) {
-//     let b = new Boid(width / 2,height / 2);
-//     flock.addBoid(b);
-//   }
-// }
-
-// function draw() {
-//   background(51);
-//   flock.run();
-// }
-
-// // Add a new boid into the System
-// function mouseDragged() {
-//   flock.addBoid(new Boid(mouseX, mouseY));
-// }
-
-// // The Nature of Code
-// // Daniel Shiffman
-// // http://natureofcode.com
-
-// // Flock object
-// // Does very little, simply manages the array of all the boids
-
-// function Flock() {
-//   // An array for all the boids
-//   this.boids = []; // Initialize the array
-// }
-
-// Flock.prototype.run = function() {
-//   for (let i = 0; i < this.boids.length; i++) {
-//     this.boids[i].run(this.boids);  // Passing the entire list of boids to each boid individually  
-//   }
-// }
-
-// Flock.prototype.addBoid = function(b) {
-//   this.boids.push(b);
-// }
-
-// // The Nature of Code
-// // Daniel Shiffman
-// // http://natureofcode.com
-
-// // Boid class
-// // Methods for Separation, Cohesion, Alignment added
-
-// function Boid(x, y) {
-//   this.acceleration = createVector(0, 0);
-//   this.velocity = createVector(random(-1, 1), random(-1, 1));
-//   this.position = createVector(x, y);
-//   this.r = 3.0;
-//   this.maxspeed = 3;    // Maximum speed
-//   this.maxforce = 0.05; // Maximum steering force
-// }
-
-// Boid.prototype.run = function(boids) {
-//   this.flock(boids);
-//   this.update();
-//   this.borders();
-//   this.render();
-// }
-
-// Boid.prototype.applyForce = function(force) {
-//   // We could add mass here if we want A = F / M
-//   this.acceleration.add(force);
-// }
-
-// // We accumulate a new acceleration each time based on three rules
-// Boid.prototype.flock = function(boids) {
-//   let sep = this.separate(boids);   // Separation
-//   let ali = this.align(boids);      // Alignment
-//   let coh = this.cohesion(boids);   // Cohesion
-//   // Arbitrarily weight these forces
-//   sep.mult(1.5);
-//   ali.mult(1.0);
-//   coh.mult(1.0);
-//   // Add the force vectors to acceleration
-//   this.applyForce(sep);
-//   this.applyForce(ali);
-//   this.applyForce(coh);
-// }
-
-// // Method to update location
-// Boid.prototype.update = function() {
-//   // Update velocity
-//   this.velocity.add(this.acceleration);
-//   // Limit speed
-//   this.velocity.limit(this.maxspeed);
-//   this.position.add(this.velocity);
-//   // Reset accelertion to 0 each cycle
-//   this.acceleration.mult(0);
-// }
-
-// // A method that calculates and applies a steering force towards a target
-// // STEER = DESIRED MINUS VELOCITY
-// Boid.prototype.seek = function(target) {
-//   let desired = p5.Vector.sub(target,this.position);  // A vector pointing from the location to the target
-//   // Normalize desired and scale to maximum speed
-//   desired.normalize();
-//   desired.mult(this.maxspeed);
-//   // Steering = Desired minus Velocity
-//   let steer = p5.Vector.sub(desired,this.velocity);
-//   steer.limit(this.maxforce);  // Limit to maximum steering force
-//   return steer;
-// }
-
-// Boid.prototype.render = function() {
-//   // Draw a triangle rotated in the direction of velocity
-//   let theta = this.velocity.heading() + radians(90);
-//   fill(127);
-//   stroke(200);
-//   push();
-//   translate(this.position.x, this.position.y);
-//   rotate(theta);
-//   beginShape();
-//   vertex(0, -this.r * 2);
-//   vertex(-this.r, this.r * 2);
-//   vertex(this.r, this.r * 2);
-//   endShape(CLOSE);
-//   pop();
-// }
-
-// // Wraparound
-// Boid.prototype.borders = function() {
-//   if (this.position.x < -this.r)  this.position.x = width + this.r;
-//   if (this.position.y < -this.r)  this.position.y = height + this.r;
-//   if (this.position.x > width + this.r) this.position.x = -this.r;
-//   if (this.position.y > height + this.r) this.position.y = -this.r;
-// }
-
-// // Separation
-// // Method checks for nearby boids and steers away
-// Boid.prototype.separate = function(boids) {
-//   let desiredseparation = 25.0;
-//   let steer = createVector(0, 0);
-//   let count = 0;
-//   // For every boid in the system, check if it's too close
-//   for (let i = 0; i < boids.length; i++) {
-//     let d = p5.Vector.dist(this.position,boids[i].position);
-//     // If the distance is greater than 0 and less than an arbitrary amount (0 when you are yourself)
-//     if ((d > 0) && (d < desiredseparation)) {
-//       // Calculate vector pointing away from neighbor
-//       let diff = p5.Vector.sub(this.position, boids[i].position);
-//       diff.normalize();
-//       diff.div(d);        // Weight by distance
-//       steer.add(diff);
-//       count++;            // Keep track of how many
-//     }
-//   }
-//   // Average -- divide by how many
-//   if (count > 0) {
-//     steer.div(count);
-//   }
-
-//   // As long as the vector is greater than 0
-//   if (steer.mag() > 0) {
-//     // Implement Reynolds: Steering = Desired - Velocity
-//     steer.normalize();
-//     steer.mult(this.maxspeed);
-//     steer.sub(this.velocity);
-//     steer.limit(this.maxforce);
-//   }
-//   return steer;
-// }
-
-// // Alignment
-// // For every nearby boid in the system, calculate the average velocity
-// Boid.prototype.align = function(boids) {
-//   let neighbordist = 50;
-//   let sum = createVector(0,0);
-//   let count = 0;
-//   for (let i = 0; i < boids.length; i++) {
-//     let d = p5.Vector.dist(this.position,boids[i].position);
-//     if ((d > 0) && (d < neighbordist)) {
-//       sum.add(boids[i].velocity);
-//       count++;
-//     }
-//   }
-//   if (count > 0) {
-//     sum.div(count);
-//     sum.normalize();
-//     sum.mult(this.maxspeed);
-//     let steer = p5.Vector.sub(sum, this.velocity);
-//     steer.limit(this.maxforce);
-//     return steer;
-//   } else {
-//     return createVector(0, 0);
-//   }
-// }
-
-// // Cohesion
-// // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
-// Boid.prototype.cohesion = function(boids) {
-//   let neighbordist = 50;
-//   let sum = createVector(0, 0);   // Start with empty vector to accumulate all locations
-//   let count = 0;
-//   for (let i = 0; i < boids.length; i++) {
-//     let d = p5.Vector.dist(this.position,boids[i].position);
-//     if ((d > 0) && (d < neighbordist)) {
-//       sum.add(boids[i].position); // Add location
-//       count++;
-//     }
-//   }
-//   if (count > 0) {
-//     sum.div(count);
-//     return this.seek(sum);  // Steer towards the location
-//   } else {
-//     return createVector(0, 0);
-//   }
-// }
-
-
+// console.log(PullToVertex(particles[1].x,particles[1].y,a1,a2));
